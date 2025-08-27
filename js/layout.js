861bce70730ac72f7836ff5755a1896ea1b6cd91
di.agm <di.creative.agm@gmail.com>
2025-08-26 21:28:38 -0600

    Update layout.js
    Signed-off-by: di.agm <di.creative.agm@gmail.com>

--- js/layout.js
+++ js/layout.js
@@ -258,34 +258,6 @@
       const colorInput = document.getElementById('colorPickerInput');
       if (colorInput) colorInput.value = selectedElement.style.color || '#000000';}
 
- /*function makeElementDraggable(el) {
-        let offsetX = 0, offsetY = 0, isDragging = false;
-        
-        el.addEventListener('mousedown', (e) => {
-        // if (e.target.classList.contains('resizer') || el.isContentEditable) return;
-        if (e.target !== el) return;
-
-        isDragging = true;
-        offsetX = e.clientX - el.offsetLeft;
-        offsetY = e.clientY - el.offsetTop;
-        
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-        });
-        
-        function onMouseMove(e) {
-        if (!isDragging) return;
-        el.style.left = (e.clientX - offsetX) + 'px';
-        el.style.top = (e.clientY - offsetY) + 'px';
-        }
-        
-        function onMouseUp() {
-        isDragging = false;
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-        }
-    }*/
-
     function makeElementDraggable(el) {
       let offsetX = 0, offsetY = 0, isDragging = false;
     
