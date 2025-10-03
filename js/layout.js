169ecce7907557a4002f363b449220ff982a7f01
di.agm <di.creative.agm@gmail.com>
2025-10-02 19:21:50 -0600

    Update layout.js
    Signed-off-by: di.agm <di.creative.agm@gmail.com>

--- js/layout.js
+++ js/layout.js
@@ -387,8 +387,8 @@ function addShapeElement(type) {
       const points = [];
       for (let i = 0; i < sides; i++) {
         const angle = (2 * Math.PI * i) / sides - Math.PI / 2;
-        const x = 50 + r * Math.cos(angle);
-        const y = 50 + r * Math.sin(angle);
+        const x = 2 * r * Math.cos(angle);
+        const y = 2 * r * Math.sin(angle);
         points.push(`${x},${y}`);
       }
       shape = document.createElementNS(svgNS, "polygon");
@@ -408,8 +408,8 @@ function addShapeElement(type) {
       for (let i = 0; i < 2 * peaks; i++) {
         const angle = (Math.PI * i) / peaks - Math.PI / 2;
         const r = i % 2 === 0 ? outerRadius : innerRadius;
-        const x = 50 + r * Math.cos(angle);
-        const y = 50 + r * Math.sin(angle);
+        const x = 2 * r * Math.cos(angle);
+        const y = 2 * r * Math.sin(angle);
         points.push(`${x},${y}`);
       }
       shape = document.createElementNS(svgNS, "polygon");
