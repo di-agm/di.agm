904f297ae5ecf59d81884da8839ba13ae05350e3
di.agm <di.creative.agm@gmail.com>
2025-10-26 21:53:38 -0600

    Update layout.js
    Signed-off-by: di.agm <di.creative.agm@gmail.com>

--- js/layout.js
+++ js/layout.js
@@ -393,7 +393,7 @@ function addShapeElement(type) {
   element.style.width = '100px';
   element.style.height = '100px';
   element.style.resize = 'both';
-  element.style.overflow = 'auto';
+  element.style.overflow = 'visible';
 
   // Create an SVG to hold the shape
   const svgNS = "http://www.w3.org/2000/svg";
@@ -469,15 +469,6 @@ function addShapeElement(type) {
     element.dataset.borderColor = shape.getAttribute('stroke');
     element.dataset.borderOpacity = 1.0;
     element.dataset.borderWidth = shape.getAttribute('stroke-width');
-    const initialBorderWidth = parseFloat(element.dataset.borderWidth); // 4
-    const buffer = initialBorderWidth * 2; // 8
-    const bufferPx = `${buffer}px`;
-
-    element.style.resize = 'both';
-    element.style.overflow = 'hidden'; // Essential for 'resize: both' to work
-    element.style.padding = bufferPx; 
-    
-    svg.style.margin = `-${bufferPx}`;
 
     svg.appendChild(shape);
     element.appendChild(svg);
@@ -1072,25 +1063,6 @@ function applyShapeStyle(element = selectedElement) {
 
     const fillType = element.dataset.fillType || 'color';
     const imageUrl = element.dataset.fillImageUrl;
-    const borderWidth = parseFloat(element.dataset.borderWidth) || 0;
-      
-      if (borderWidth > 0) {
-          const buffer = borderWidth * 2;
-          const bufferPx = `${buffer}px`;
-          
-          element.style.padding = bufferPx; 
-          
-          const svg = element.querySelector('svg');
-          if (svg) {
-              svg.style.margin = `-${bufferPx}`;
-          }
-      } else {
-          element.style.padding = '0';
-          const svg = element.querySelector('svg');
-          if (svg) {
-              svg.style.margin = '0';
-          }
-      }
 
     if (fillType === 'image' && imageUrl) {
         let defs = svg.querySelector('defs');
