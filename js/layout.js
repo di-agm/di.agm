d51236b7d7f04fe8b068ba90987b2ac747a79f62
di.agm <di.creative.agm@gmail.com>
2025-10-29 23:47:17 -0600

    Update layout.js
    Signed-off-by: di.agm <di.creative.agm@gmail.com>

--- js/layout.js
+++ js/layout.js
@@ -545,6 +545,7 @@ function selectElement(element) {
 
     if (toolbar) {
         toolbar.style.display = 'flex'; 
+        toolbar.style.position = 'absolute';
         toolbar.style.left = `${rect.left + rect.width / 2 - toolbar.offsetWidth / 2}px`;
         toolbar.style.top = `${rect.bottom - containerRect.top + 5}px`;
     }
