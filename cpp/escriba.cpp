aba15a05c48425e8a6abfa2bdb0b7443e1be7308
di.agm <di.creative.agm@gmail.com>
2026-07-24 10:43:59 -0600

    Add initial C++ program that prints a greeting
    Signed-off-by: di.agm <di.creative.agm@gmail.com>

---
+++ cpp/escriba.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // Aquí es donde el programa ejecuta las instrucciones
+    std::cout << "¡Hola! Esta es mi primera prueba en C++" << std::endl;
+    
+    return 0;
+}
